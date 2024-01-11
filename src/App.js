ac56430062c22e3341b60ffafe6a3bad41a21044
Raju Saha <rajusaha010693@gmail.com>
2024-01-11 22:23:43 +0530

    basic dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
-import Otp from "./components/Otp";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 const App = () => {
-  return <Otp />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userid" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="devices">
+            <Route index element={<List />} />
+            <Route path=":deviceid" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
