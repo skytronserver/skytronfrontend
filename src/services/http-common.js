551df2e23e4cd49c1908a58fe0c1e2d46419552a
Raju Saha <rajusaha010693@gmail.com>
2024-01-27 23:35:26 +0530

    Admin Update Action added

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://216.10.244.243:2000",
+  baseURL: "//216.10.244.243:2000",
   headers: {
     "Content-type": "application/json",
     "Authorization":"Token 31353e8296e9b40f88cdda7a080b5e286fa88c2f"
