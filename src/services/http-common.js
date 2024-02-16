cfc91738766b42e756f39f1e41f96aa413cad958
Raju Saha <rajusaha010693@gmail.com>
2024-02-16 19:24:17 +0530

    changed baseurl

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "//216.10.244.243:2000",
+  baseURL: "https://skytrack.tech:2000/",
   headers: {
     "Content-type": "application/json",
     "Authorization": "Token 31353e8296e9b40f88cdda7a080b5e286fa88c2f",
