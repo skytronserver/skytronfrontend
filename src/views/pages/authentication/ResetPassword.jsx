983a500f76accc3b91c6f72f8f48e5e67a7fca0c
Raju Saha <rajusaha010693@gmail.com>
2024-05-19 15:50:29 +0530

    added reset link

---
+++ src/views/pages/authentication/ResetPassword.jsx
@@ -0,0 +1,175 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Divider, Grid, Stack, Typography, useMediaQuery, Button, TextField } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import AuthWrapper1 from "./AuthWrapper1";
+import AuthCardWrapper from "./AuthCardWrapper";
+import AuthFooter from "../../../ui-component/cards/AuthFooter";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+const ResetPassword = () => {
+  const { reset_token } = useParams();
+  const theme = useTheme();
+  const matchDownSM = useMediaQuery(theme.breakpoints.down("md"));
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [arePasswordsMatch, setArePasswordsMatch] = useState(false);
+
+  const handlePasswordChange = (value) => {
+    setPassword(value); 
+  };
+
+  const handleConfirmPasswordChange = (value) => {
+    setConfirmPassword(value);
+  };
+  useEffect(()=>{
+    setArePasswordsMatch(password === confirmPassword);
+  },[password,confirmPassword])
+  const handleResetPassword = async () => {
+    if(password!=''){
+        setLoading(true)
+        try {
+            await axios.post('https://skytrack.tech:2000/api/password_reset', { reset_token, new_password: password },{
+                headers:{
+                    "Content-type": "application/json",
+                    "Authorization": "Token "+reset_token,
+                  }
+            });
+            window.location.href="https://www.skytrack.tech/mis"
+          } catch (err) {
+            console.error(err);
+            setError('Failed to reset password');
+          }finally{
+            setLoading(false)
+          }
+    }else{
+        setError('Failed to reset password or password field is empty');
+    }
+    
+  };
+
+  return (
+    <AuthWrapper1>
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-end"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            sx={{ minHeight: "calc(100vh - 68px)" }}
+          >
+            <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+              {loading && (
+                <Grid
+                  container
+                  spacing={2}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Grid
+                    item
+                    xs={12}
+                    container
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <CircularProgress color="secondary" />
+                  </Grid>
+                </Grid>
+              )}
+              {(
+                <AuthCardWrapper>
+                  <Grid
+                    container
+                    spacing={2}
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    {/* Existing content */}
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        direction={matchDownSM? "column-reverse" : "row"}
+                        alignItems="center"
+                        justifyContent="center"
+                      >
+                        <Grid item>
+                          <Stack
+                            alignItems="center"
+                            justifyContent="center"
+                            spacing={1}
+                          >
+                            <Typography
+                              color={theme.palette.secondary.main}
+                              gutterBottom
+                              variant={matchDownSM? "h3" : "h2"}
+                            >
+                             SKYTRON
+                            </Typography>
+                            <Typography
+                              variant="caption"
+                              fontSize="16px"
+                              textAlign={matchDownSM? "center" : "inherit"}
+                            >
+                              Reset Password
+                            </Typography>
+                          </Stack>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography align="center">
+                        <TextField
+                          label="New Password"
+                          type="password"
+                          value={password}
+                          onChange={(e) => handlePasswordChange(e.target.value)}
+                          fullWidth
+                        />
+                        <br/><br/>
+                        <TextField
+                          label="Confirm New Password"
+                          type="password"
+                          value={confirmPassword}
+                          onChange={(e) => handleConfirmPasswordChange(e.target.value)}
+                          fullWidth
+                          error={!arePasswordsMatch} // Set error state based on password match
+                          helperText={!arePasswordsMatch? "Passwords do not match" : ""} // Show error message if passwords do not match
+                        />
+                        <br/><br/>
+                        <Button
+                          color="primary"
+                          size="large"
+                          type="button"
+                          variant="contained"
+                          onClick={handleResetPassword}
+                          disabled={!arePasswordsMatch} // Disable button if passwords don't match
+                        >
+                          Reset Password
+                        </Button>
+                      </Typography>
+                    </Grid>
+                    {error && <Typography style={{ color: "red", textAlign: 'center' }}>{error}</Typography>}
+                  </Grid>
+                </AuthCardWrapper>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+          <AuthFooter />
+        </Grid>
+      </Grid>
+    </AuthWrapper1>
+  );
+};
+
+export default ResetPassword;
