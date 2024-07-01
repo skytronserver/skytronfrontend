ebed120dc5405d818244b6aeb5cc5517df12b26c
Raju Saha <rajusaha010693@gmail.com>
2024-07-01 22:02:20 +0530

    added sliding background images

--- src/layout/HomepageLayout/index.js
+++ src/layout/HomepageLayout/index.js
@@ -1,20 +1,22 @@
 import { Outlet } from 'react-router-dom';
-import React, { useState } from 'react';
-import backgroundImage from "../../assets/images/GPSImagee.jpg";
+import React, { useState,useEffect } from 'react';
+import "./homePageStyle.css";
+// import backgroundImage from "../../assets/images/GPSImagee.jpg";
 import HomeHeader from './HomeHeader';
 import HomeFooter from './HomeFooter';
+import image2 from "../../assets/images/image2.jpg";
+import image1 from "../../assets/images/image1.jpg";
+import image3 from "../../assets/images/image3.png";
+import image4 from "../../assets/images/image4.jpg";
 
-const myStyle = {
-  backgroundImage: `url(${backgroundImage})`,
-  minHeight: '100vh',
-  backgroundSize: "cover, contain",
-  backgroundRepeat: "no-repeat",
-};
+const images = [image1, image2, image3,image4];
+const delay = 6000; // Time delay for slide transition
+
 // ==============================|| HOME PAGE LAYOUT ||============================== //
 
 const HomepageLayout = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
-
+  const [currentSlide, setCurrentSlide] = useState(0);
   const toggleDrawer = (open) => (event) => {
     if (
       event.type === 'keydown' &&
@@ -25,13 +27,33 @@ const HomepageLayout = () => {
 
     setDrawerOpen(open);
   };
-
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+    }, delay);
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div style={myStyle}>
+    <div className="layout-container">
+    <div className="slider">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`slide ${index === currentSlide ? 'active' : ''}`}
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      ))}
+    </div>
+    <div className="header">
       <HomeHeader isDrawerOpen={isDrawerOpen} setDrawerOpen={setDrawerOpen} toggleDrawer={toggleDrawer} />
-      <Outlet/>
+    </div>
+    <div className="content">
+      <Outlet />
+    </div>
+    <div className="footer">
       <HomeFooter />
     </div>
+  </div>
 );
 }
 export default HomepageLayout;
