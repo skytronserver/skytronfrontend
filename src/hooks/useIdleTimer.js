246cd03c09b7c291e4ef42e14ab27d39c7a69993
Raju Saha <rajusaha010693@gmail.com>
2024-07-16 23:19:56 +0530

    role based dashboard api added

--- src/hooks/useIdleTimer.js
+++ src/hooks/useIdleTimer.js
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react';
-
+import { debounce } from 'helper';
 const useIdleTimer = (logoutCallback, timeout = 300000) => {
-  //take state to check if the user is idle or not 
   const [isIdle, setIsIdle] = useState(false);
 
   useEffect(() => {
-    //variable for storing the timeout
+    if (typeof logoutCallback !== 'function') {
+      console.error('logoutCallback must be a function');
+      return;
+    }
+
     let timeoutId;
     const handleActivity = () => {
-      //when ever user interact we make is idle to false and clear the timeout
       setIsIdle(false);
       clearTimeout(timeoutId);
-      //else if the user doesn't interact for certain amount of time then it will update the state as it is idle and then the logout can be called
       timeoutId = setTimeout(() => setIsIdle(true), timeout);
     };
 
+    const debounceHandleActivity = debounce(handleActivity, 200);
+
     const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
 
-    events.forEach((event) => window.addEventListener(event, handleActivity));
+    events.forEach((event) => window.addEventListener(event, debounceHandleActivity));
 
     handleActivity(); // Initialize the timeout
 
     return () => {
       clearTimeout(timeoutId);
-      events.forEach((event) => window.removeEventListener(event, handleActivity));
+      events.forEach((event) => window.removeEventListener(event, debounceHandleActivity));
     };
-  }, [timeout]);
+  }, [timeout, logoutCallback]);
 
   useEffect(() => {
     if (isIdle) {
@@ -35,5 +38,4 @@ const useIdleTimer = (logoutCallback, timeout = 300000) => {
 
   return isIdle;
 };
-
 export default useIdleTimer;
