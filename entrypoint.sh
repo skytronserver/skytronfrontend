5d7dbafa19b27fc227f717aa55c1970a41e249eb
AnkurDutta21 <daxxz2212@gmail.com>
2025-03-11 17:52:27 +0530

    fix: removed static links

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/shset 
--e
-# Replace environment variables in the Nginx confenvsubst 
-'${API_DOMAIN} ${BHUVAN_DOMAIN} ${FONTS_DOMAIN} ${FONTS_STATIC_DOMAIN} ${OSM_DOMAIN}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
-# Execute the CMD from the Dockerfileexec 
-"$@"
+#!/bin/sh
+set -e
+
+# Replace environment variables in the Nginx conf
+envsubst '${API_DOMAIN} ${BHUVAN_DOMAIN} ${FONTS_DOMAIN} ${FONTS_STATIC_DOMAIN} ${OSM_DOMAIN}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf    
+
+# Reload nginx instead of starting a new instance
+nginx -s reload || nginx -g 'daemon off;'
