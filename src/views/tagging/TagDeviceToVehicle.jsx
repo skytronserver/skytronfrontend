a10108aa0bcd0c620518382443dddeed2c91ab37
AnkurDutta21 <daxxz2212@gmail.com>
2025-03-13 14:29:59 +0530

    fix:updated tagging live map

--- src/views/tagging/TagDeviceToVehicle.jsx
+++ src/views/tagging/TagDeviceToVehicle.jsx
@@ -25,6 +25,7 @@ import "../forms/form.css";
 import CustomStepper from "../../ui-component/CustomStepper";
 import AutoHideAlert from "../../ui-component/AutoHideAlert";
 import DisplayTable from "../../ui-component/DisplayTable";
+import MapComponent from "views/direct/LiveMap";
 const steps = [
   { label: "Tag Device to Vehicle", name: "Step 1" },
   { label: "Dealer Verification", name: "Step 2" },
@@ -62,7 +63,8 @@ function TagDeviceToVehicle() {
     type: "success",
     message: "",
   });
- 
+
+
   const [ownerDetails, setOwnerDetails] = useState({
     name: "",
     email: "",
@@ -83,29 +85,29 @@ function TagDeviceToVehicle() {
     esim_validity: "",
     esim_provider: "",
     model: "",
-    
+
   });
- 
-  const [vahanDetails,setVahanDetails]=useState({
-    owner_name:"",
+
+  const [vahanDetails, setVahanDetails] = useState({
+    owner_name: "",
     email: "",
-    mobile: "", 
-    device_serial_no:"",
-    ICCID:"",
-    IMEI:"",
-    vehicle_reg_no:"",
-    vehicle_make:"", 
-    vehicle_model:"",
-    engine_no:"",
-    chassis_no:"",
-    vehicle_class:"",
-    date_of_registration:"",
-    device_activation_status:"",
-    fitment_centre_name:"",
-    GNSS_constellation_code:"",
-    tac_no:"",
-    tac_valid_upto:"",
-    
+    mobile: "",
+    device_serial_no: "",
+    ICCID: "",
+    IMEI: "",
+    vehicle_reg_no: "",
+    vehicle_make: "",
+    vehicle_model: "",
+    engine_no: "",
+    chassis_no: "",
+    vehicle_class: "",
+    date_of_registration: "",
+    device_activation_status: "",
+    fitment_centre_name: "",
+    GNSS_constellation_code: "",
+    tac_no: "",
+    tac_valid_upto: "",
+
   })
 
   useEffect(() => {
@@ -146,12 +148,12 @@ function TagDeviceToVehicle() {
       device_id: deviceId,
     };
     try {
-      if(type==='dealer'){await TaggingService.tagVerifyDealerOtp(OtpData);}
-      if(type==='owner'){await TaggingService.tagVerifyOwnerOtp(OtpData);}
-      if(type==='finalOwner'){await TaggingService.verifyTagVerifyOwnerOtpFinal(OtpData);}
-      setActiveStep(prevActiveStep=>prevActiveStep+1)
-      setDismissibleAlert(prev=>({...prev,isOpen:true,message:'OTP has been successfully verified',type:'success'}));
-   
+      if (type === 'dealer') { await TaggingService.tagVerifyDealerOtp(OtpData); }
+      if (type === 'owner') { await TaggingService.tagVerifyOwnerOtp(OtpData); }
+      if (type === 'finalOwner') { await TaggingService.verifyTagVerifyOwnerOtpFinal(OtpData); }
+      setActiveStep(prevActiveStep => prevActiveStep + 1)
+      setDismissibleAlert(prev => ({ ...prev, isOpen: true, message: 'OTP has been successfully verified', type: 'success' }));
+
     } catch (error) {
       setDismissibleAlert((prev) => ({
         ...prev,
@@ -170,10 +172,10 @@ function TagDeviceToVehicle() {
       device_id: deviceId,
     };
     try {
-      if(type==='owner'){await TaggingService.tagSendOwnerOtp(otpData);}
-      if(type==='finalOwner'){await TaggingService.sendTagSendOwnerOtpFinal(otpData);}
-      setActiveStep(prevActiveStep=>prevActiveStep+1)
-      setDismissibleAlert(prev=>({...prev,isOpen:true,message:'Vehicle Owner OTP has been sent successfully',type:'success'}));
+      if (type === 'owner') { await TaggingService.tagSendOwnerOtp(otpData); }
+      if (type === 'finalOwner') { await TaggingService.sendTagSendOwnerOtpFinal(otpData); }
+      setActiveStep(prevActiveStep => prevActiveStep + 1)
+      setDismissibleAlert(prev => ({ ...prev, isOpen: true, message: 'Vehicle Owner OTP has been sent successfully', type: 'success' }));
     } catch (error) {
       console.error("Error while submitting data", error.message);
       setDismissibleAlert((prev) => ({
@@ -197,9 +199,9 @@ function TagDeviceToVehicle() {
       const device = response?.data?.Skytrack_data?.device || response?.Skytrack_data?.device;
       const owner = response?.data?.Skytrack_data?.vehicle_owner.users[0] || response?.Skytrack_data?.vehicle_owner.users[0];
       const vehicle = response?.data?.Skytrack_data || response?.Skytrack_data;
-      const vahanStringData=response?.data?.vahan_data;
-      const vahanJSONData=JSON.parse(vahanStringData);
-      const vahanData=vahanJSONData?.VltdDetailsDobj
+      const vahanStringData = response?.data?.vahan_data;
+      const vahanJSONData = JSON.parse(vahanStringData);
+      const vahanData = vahanJSONData?.VltdDetailsDobj
       setGetMap((prev) => ({
         ...prev,
         regno: vehicle.vehicle_reg_no,
@@ -229,37 +231,37 @@ function TagDeviceToVehicle() {
       }));
       setVahanDetails((prev) => ({
         ...prev,
-        chassis_no:vahanData?.chassisNo,
-        date_of_registration:vahanData?.dateOfRegistration,
-        device_activation_status:vahanData?.deviceActivationStatus,
-        device_serial_no:vahanData?.deviceSerialno,
-        engine_no:vahanData?.engineNo,
-        fitment_centre_name:vahanData?.fitmentCentreName,
-        GNSS_constellation_code:vahanData?.gnssConstellationCode,
-        ICCID:vahanData?.iccId,
-        IMEI:vahanData?.imeiNo,
-        vehicle_make:vahanData?.makerName, 
-        vehicle_model:vahanData?.modelName,
-        owner_name:vahanData?.ownerName, 
-        vehicle_reg_no:vahanData?.regnNo,
-        tac_no:vahanData?.tacNo,
-        tac_valid_upto:vahanData?.tacValidUpto,
-        vehicle_class:vahanData?.vehClass,
+        chassis_no: vahanData?.chassisNo,
+        date_of_registration: vahanData?.dateOfRegistration,
+        device_activation_status: vahanData?.deviceActivationStatus,
+        device_serial_no: vahanData?.deviceSerialno,
+        engine_no: vahanData?.engineNo,
+        fitment_centre_name: vahanData?.fitmentCentreName,
+        GNSS_constellation_code: vahanData?.gnssConstellationCode,
+        ICCID: vahanData?.iccId,
+        IMEI: vahanData?.imeiNo,
+        vehicle_make: vahanData?.makerName,
+        vehicle_model: vahanData?.modelName,
+        owner_name: vahanData?.ownerName,
+        vehicle_reg_no: vahanData?.regnNo,
+        tac_no: vahanData?.tacNo,
+        tac_valid_upto: vahanData?.tacValidUpto,
+        vehicle_class: vahanData?.vehClass,
       }));
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      setDismissibleAlert(prev=>({...prev,isOpen:true,message:'Vahan Details are successfully fetched',type:'success'}));
-    
+      setDismissibleAlert(prev => ({ ...prev, isOpen: true, message: 'Vahan Details are successfully fetched', type: 'success' }));
+
     } catch (error) {
       console.error("Error :", error?.message);
-      let errorString="Something went wrong! Please try after sometimes or check your details";
-      if(error?.response){
-        const errorObject=error?.response?.data || '';
-        errorString = errorObject!=='' && Object.values(errorObject).flat().join(" ");
+      let errorString = "Something went wrong! Please try after sometimes or check your details";
+      if (error?.response) {
+        const errorObject = error?.response?.data || '';
+        errorString = errorObject !== '' && Object.values(errorObject).flat().join(" ");
       }
       setDismissibleAlert((prev) => ({
         ...prev,
         isOpen: true,
-        message:errorString,
+        message: errorString,
         type: "error",
       }));
     } finally {
@@ -269,7 +271,8 @@ function TagDeviceToVehicle() {
   const retriveMapData = async () => {
     setLoading((prev) => ({ ...prev, loader: true }));
     try {
-      const retriveData = await HomePageService.getLiveTracking(getMap);
+      const retriveData = await HomePageService.getLiveTracking_data(getMap);
+      console.log("Retrieved Map Data:", retriveData.data);
       setHtmlContent(retriveData.data);
       setMapLoaded(true);
 
@@ -281,7 +284,7 @@ function TagDeviceToVehicle() {
       }));
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
     } catch (error) {
-      console.log(error);
+      console.log("Error retrieving map data:", error);
       setDismissibleAlert((prev) => ({
         ...prev,
         isOpen: true,
@@ -312,25 +315,25 @@ function TagDeviceToVehicle() {
     setSubmitting(true);
     setLoading((prev) => ({ ...prev, loader: true }));
     try {
-      const response=await TaggingService.tagDeviceToVehicle(values);
+      const response = await TaggingService.tagDeviceToVehicle(values);
       resetForm(taggingInitials);
       setDeviceId(response?.data?.data?.device);
-      setActiveStep(prevActiveStep=>prevActiveStep+1);
-      setDismissibleAlert(prev=>({...prev,isOpen:true,message:'Successfully OTP has been sent to your registered mobile number',type:'success'}));
+      setActiveStep(prevActiveStep => prevActiveStep + 1);
+      setDismissibleAlert(prev => ({ ...prev, isOpen: true, message: 'Successfully OTP has been sent to your registered mobile number', type: 'success' }));
     } catch (error) {
       console.log(error);
-      const errorData=error?.response?.data?.detail || error?.response?.data?.error
-      const message=errorData ? "Error Details "+ errorData :'Something went wrong! Please try after sometimes or check your details';
+      const errorData = error?.response?.data?.detail || error?.response?.data?.error
+      const message = errorData ? "Error Details " + errorData : 'Something went wrong! Please try after sometimes or check your details';
       if (error?.message === "Network Error") {
         setError((prev) => ({ ...prev, api: true }));
       } else {
-        
+
         setError((prev) => ({ ...prev, normal: true }));
       }
       setDismissibleAlert((prev) => ({
         ...prev,
         isOpen: true,
-        message:message,
+        message: message,
         type: "error",
       }));
     } finally {
@@ -548,36 +551,36 @@ function TagDeviceToVehicle() {
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                <Grid container spacing={1}>
-  <Grid 
-    item 
-    xs={6} 
-    sx={{ 
-      borderRadius: '8px 0 0 8px', 
-      border: '1px solid #f0f0f0', 
-      backgroundColor: 'white'
-    }}
-  >
-    <DisplayTable
-      values={ownerDetails}
-      title="Details as in Skytron"
-    />
-  </Grid>
-  <Grid 
-    item 
-    xs={6} 
-    sx={{ 
-      borderRadius: '0 8px 8px 0', 
-      border: '1px solid #f0f0f0', 
-      backgroundColor: 'white'
-    }}
-  >
-    <DisplayTable
-      values={vahanDetails}
-      title="Details as in Vahan"
-    />
-  </Grid>
-</Grid>
+                  <Grid container spacing={1}>
+                    <Grid
+                      item
+                      xs={6}
+                      sx={{
+                        borderRadius: '8px 0 0 8px',
+                        border: '1px solid #f0f0f0',
+                        backgroundColor: 'white'
+                      }}
+                    >
+                      <DisplayTable
+                        values={ownerDetails}
+                        title="Details as in Skytron"
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={6}
+                      sx={{
+                        borderRadius: '0 8px 8px 0',
+                        border: '1px solid #f0f0f0',
+                        backgroundColor: 'white'
+                      }}
+                    >
+                      <DisplayTable
+                        values={vahanDetails}
+                        title="Details as in Vahan"
+                      />
+                    </Grid>
+                  </Grid>
 
                 </Grid>
               </Grid>
@@ -601,17 +604,11 @@ function TagDeviceToVehicle() {
                 </Grid>
                 <Grid item xs={12}>
                   {mapLoaded && (
-                    <section>
-                      <iframe
-                        title="HTML Content"
-                        srcDoc={htmlContent} // Set the HTML content as srcDoc
-                        style={{
-                          width: "100%",
-                          height: "500px",
-                          border: "1px solid #ccc",
-                        }}
+                      <MapComponent
+                        gpsData={htmlContent?.data}
+                        width="100%"
+                        height="600px"
                       />
-                    </section>
                   )}
                 </Grid>
               </Grid>
