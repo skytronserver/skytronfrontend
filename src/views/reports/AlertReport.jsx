ab332b705a54ee05b505d1b41e17d397d74c27fb
Kamini <kamini200297@gmail.com>
2026-02-13 12:33:08 +0530

    replace unauthorised option  in alert report

--- src/views/reports/AlertReport.jsx
+++ src/views/reports/AlertReport.jsx
@@ -58,7 +58,7 @@ const AlertReport = () => {
     { value: 'Idling', label: 'Idling' },
     { value: 'OfflineDevice', label: 'OfflineDevice' },
     { value: 'Overtime', label: 'Overtime' },
-    { value: 'UnauthorizedStop', label: 'UnauthorizedStop' },
+    { value: 'UnauthorizedStop', label: 'Prohibited Area' },
     { value: 'UnauthorizedSkip', label: 'UnauthorizedSkip' },
     { value: 'NetworkLoss', label: 'NetworkLoss' },
     { value: 'GPSLoss', label: 'GPSLoss' },
