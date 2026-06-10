102253e7b9d8b6644405ac04dcb0aab0effb5a11
Vicky Kumar <vickyray115@gmail.com>
2026-06-10 18:13:44 +0530

    on same routes copy of every page with same data for police pages created

---
+++ src/views/direct/SOSPoliceExe.jsx
@@ -0,0 +1,34 @@
+// material-ui
+import React from 'react';
+// project imports
+import MainCard from '../../ui-component/cards/MainCard';
+import HomePageService from "../../services/HomePage"
+import { useEffect, useState } from 'react';
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const SOSExe = () => {
+  const [load, setLoad] = useState(false)
+  const [htmlContent, setHtmlContent] = useState('');
+  useEffect(() => {
+    const retriveSOSData = async () => {
+      try {
+        //const retriveData = await HomePageService.getSOSDataExe();
+        //setHtmlContent(retriveData.data)  
+        setLoad(true)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    retriveSOSData();
+  }, [])
+  return (
+    <MainCard>
+      <iframe
+        title="HTML Content"
+        srcDoc={htmlContent} // Set the HTML content as srcDoc
+        style={{ width: '100%', height: '500px', border: '1px solid #ccc' }}
+      />
+    </MainCard>
+  );
+}
+export default SOSExe;
---
+++ src/views/reports/policeAlertList.jsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import React from "react";
+import { Grid } from "@mui/material";
+import { gridSpacing } from "../../store/constant";
+import ReportServices from "../../services/ReportServices";
+import { useEffect} from "react";
+import { useTranslation } from 'react-i18next';
+
+const AlertList = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const on_debug=false
+  
+  useEffect(() => {
+    const fetchAlertList = async () => {
+      // const retriveData = await ReportServices.alertList();
+    };
+    fetchAlertList();
+  }, [dispatch]);
+if(on_debug){
+console.log("AlertList")  
+}
+  // const alertList=useSelector((state)=>state.reports.alertList);
+  return (
+    <Grid container spacing={gridSpacing}>
+    </Grid>
+  );
+};
+
+export default AlertList;
