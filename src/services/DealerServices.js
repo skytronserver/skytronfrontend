5fe5948da1d0ce039cbdfa602a8b276859643e9c
Vicky Kumar <vickyray115@gmail.com>
2026-09-03 13:09:56 +0530

    Create Dealer code updated

--- src/services/DealerServices.js
+++ src/services/DealerServices.js
@@ -1,12 +1,21 @@
 import { getAxiosInstance } from './axiosInstance';  
-const dealerUser=(userData)=>{
-  const http = getAxiosInstance(); 
-    return http.post(`/api/dealer/create_dealer/`,userData,{
-        headers: {
-          'Content-type': 'multipart/form-data',
-        }
-      })
-}
+// const dealerUser=(userData)=>{
+//   const http = getAxiosInstance(); 
+//     return http.post(`/api/dealer/create_dealer/`,userData,{
+//         headers: {
+//           'Content-type': 'multipart/form-data',
+//         }
+//       })
+// }
+
+const dealerUser = (formData) => {
+  const http = getAxiosInstance();
+
+  return http.post(
+    "/api/dealer/create_dealer/",
+    formData
+  );
+};
 const dealerList = (id) => {
   const http = getAxiosInstance(); 
   return http.post("/api/dealer/filter_dealer/",id);
